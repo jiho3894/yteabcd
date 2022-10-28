51a2e08393e4c5265d419be85590b9dd20ff51d9
김지호 <crsn1111@gmail.com>
2022-10-29 01:31:05 +0900

    [feat] 검색 컴포넌트

---
+++ src/components/Search.tsx
@@ -0,0 +1,31 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dispatch, SetStateAction, useRef } from "react";
+
+interface props {
+  setText: Dispatch<SetStateAction<string>>;
+}
+
+const Search = ({ setText }: props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClickInput = () => {
+    inputRef.current?.focus();
+  };
+  return (
+    <div
+      onClick={onClickInput}
+      className="w-80 space-x-2 rounded-md border-[1px] p-3 text-sm text-gray-400 duration-150  hover:border-blue-400 active:border-blue-400"
+    >
+      <FontAwesomeIcon icon={faSearch} />
+      <input
+        ref={inputRef}
+        onChange={(e) => setText(e.target.value)}
+        type="text"
+        placeholder="검색어를 입력하세요"
+        className="w-64"
+      />
+    </div>
+  );
+};
+
+export default Search;
